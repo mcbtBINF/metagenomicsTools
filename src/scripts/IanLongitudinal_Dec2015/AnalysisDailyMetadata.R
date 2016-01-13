ff7c07e17664de896878f67b1cc603ca0f6fba41
mcb1983 <mbrown67@uncc.edu>
2016-01-13 08:47:16 -0500

    Added in mixed model and autocorrelation code stub

--- src/scripts/IanLongitudinal_Dec2015/AnalysisDailyMetadata.R
+++ src/scripts/IanLongitudinal_Dec2015/AnalysisDailyMetadata.R
@@ -7,6 +7,7 @@ library("vegan")
 library("lmtest")
 library("pscl")
 library("nlme")
+library("gtools")
 
 taxaLevels <- c("phylum","class","order","family","genus")
 #Right Now I'm being lazy with the changing of the variable names
@@ -37,6 +38,9 @@ for(t in taxaLevels )
         energyPValues <- vector()
 
         fullPValues <- list()
+        noIntPValues <- list()
+        BMIPValues <- list()
+        EnergyPValues <- list()
 
         #Patients to colors
 	colors <- vector()
@@ -61,16 +65,15 @@ for(t in taxaLevels )
                                         #Bad hardcode selectors
         #Choosing a patient
                                         #Patient A
-        myT<- myT[seq(1,67),]
+#        myT<- myT[seq(1,67),]
 
                                         #Patient B
 #        myT<- myT[seq(68,114),]
                                         #Patient C
 #        myT<- myT[seq(115,147),]
                                         #Excludes other measurement columns
-
-        pval.list <- list()
-        rsquared.list <- list()
+        # Should reliably do this even for the mixed case
+        myT <- myT[mixedorder(myT[,1]),]
 
         for( i in 2:(ncol(myT) - 14) )
             {
@@ -81,22 +84,25 @@ for(t in taxaLevels )
                                                 Day<-myT$Day
                         ImputedBMI<-myT$Imputed.BMI
                                                 EnergyIntake<-myT$Energy.Intake..kcal.day.
-                                                taxaType <- myT[,i]
+                                                taxaType <- as.numeric(myT[,i])
 
+                                                myFrame <- data.frame(colors, taxaType, Day, ImputedBMI, EnergyIntake)
                                              #   if(modelNum == 0) {
                                         # Simple model which doesn't explain much of the variance
 
+                                                fullModelLME <- lme(taxaType~ Day, method="REML", random = ~1|factor(colors), data = myFrame)
+
                                                 myLm <- lm(taxaType ~ Day, x=TRUE)
                                             #}
-
-#                                                pval.list[[i]]<-summary(myLm)$coefficients[,4]
+       #                                                pval.list[[i]]<-summary(myLm)$coefficients[,4]
  #                                               rsquared.list[[i]]<-summary(myLm)$r.squared
 
                                              #   anova(myLm)
                                               #  plot(Day, taxaType)
                                                # abline(myLm)
                                         #Simple No Interaction
-                                                BMILm <- lm(taxaType ~ ImputedBMI + EnergyIntake, x = TRUE)
+                                                BMIlm <- lm(taxaType ~ ImputedBMI, x = TRUE)
+                                                Energylm <- lm(taxaType ~ EnergyIntake, x=TRUE)
                                                 myNoInteractLm <- lm(taxaType ~ Day + ImputedBMI + EnergyIntake, x = TRUE)
 
                                         #All possible situation
@@ -105,8 +111,39 @@ for(t in taxaLevels )
                                                # }
                                         #Suggested from fullModel
                                                 mySuggested <- lm(taxaType ~ Day + ImputedBMI*EnergyIntake, x = TRUE)
-                                                timePValues[index]<-summary(myLm)$coefficients[,4][[2]]
 
+                                        # Second corner
+     #                                           time2ndOrderPseud <- lm( myT$LogPseudo ~ poly( myT$sampleDays,2), x=TRUE)
+                                                # This is probably just developed for the simple regular intervals of time points.
+
+                                        # Autocorrelation Code
+      #                                          M1 <- gls( taxaType ~ EnergyIntake , data= myFrame )
+#M2 <- gls( taxaType ~ EnergyIntake + Day , data= myFrame )
+#M3 <- gls( taxaType ~ EnergyIntake + Day , data= myFrame , correlation = corCompSymm(form=~Day ))
+#M4 <- gls( taxaType ~ EnergyIntake + Day , data= myFrame , correlation = corAR1(form=~Day ))
+#M5 <- gls( taxaType ~ EnergyIntake * Day , data= myFrame  )
+
+#M6 <- gls( taxaType ~ EnergyIntake + poly(Day,2) , data= myFrame )
+#M7 <- gls( taxaType ~ EnergyIntake + poly(Day,2) , data= myFrame ,correlation = corCompSymm(form=~Day ))
+#M8 <- gls( taxaType ~ EnergyIntake + poly(Day,2) , data= myFrame ,correlation = corAR1(form=~Day ))
+
+
+#E <- residuals( M3, type="normalized")
+#acf(E)
+
+#anova(M2,M3,M4, M5,M6)
+#anova(M6,M7,M8)
+#summary(M2)
+#summary(M4)
+#plot(residuals(M2), myFrame$Day)
+#boxplot( residuals(M2) ~ myFrame$EnergyIntake)
+
+
+                                                timePValues[index]<-summary(myLm)$coefficients[,4][[2]]
+                                                BMIPValues[index]<-list(summary(BMIlm)$coefficients[,4][-1])
+EnergyPValues[index]<-list(summary(Energylm)$coefficients[,4][-1])
+
+noIntPValues[index]<-list(summary(myNoInteractLm)$coefficients[,4][-1])
                                                 fullPValues[index]<-list(summary(fullModel)$coefficients[,4][-1])
 
                                         #print(summary(myLm)$coefficients[,4][[2]])
@@ -190,8 +227,36 @@ for(t in taxaLevels )
 
 
 	dFrame <- data.frame( names, timePValues) # ,interactionPValues )
-       dFrame <- dFrame [order(dFrame$timePValues),]
+        dFrame <- dFrame [order(dFrame$timePValues),]
 	dFrame$adjTime<-  p.adjust( dFrame$timePValues , method = "BH" )
+
+        BMIPV.df <- data.frame(BMIPValues)
+        BMIPV.df <- t(BMIPV.df)
+        dFrameBMI <- data.frame(names, BMIPV.df)
+        rownames(dFrameBMI) <- names
+        for (m in 2:dim(dFrameBMI)[2]) {
+            dFrameBMI[,dim(dFrameBMI)[2] + 1] <- p.adjust(dFrameBMI[,m], method = "BH")
+            colnames(dFrameBMI)[ncol(dFrameBMI)]<-paste0("adj",colnames(dFrameBMI)[m])
+        }
+
+        EnergyPV.df <- data.frame(EnergyPValues)
+        EnergyPV.df <- t(EnergyPV.df)
+        dFrameEnergy <- data.frame(names, EnergyPV.df)
+        rownames(dFrameEnergy) <- names
+        for (m in 2:dim(dFrameEnergy)[2]) {
+            dFrameEnergy[,dim(dFrameEnergy)[2] + 1] <- p.adjust(dFrameEnergy[,m], method = "BH")
+            colnames(dFrameEnergy)[ncol(dFrameEnergy)]<-paste0("adj",colnames(dFrameEnergy)[m])
+        }
+
+
+        noIntPV.df <- data.frame(noIntPValues)
+        noIntPV.df <- t(noIntPV.df)
+        dFrameNoInt <- data.frame(names, noIntPV.df)
+        rownames(dFrameNoInt)<- names
+        for (m in 2:dim(dFrameNoInt)[2]) {
+            dFrameNoInt[,dim(dFrameNoInt)[2] + 1] <- p.adjust(dFrameNoInt[,m], method = "BH")
+            colnames(dFrameNoInt)[ncol(dFrameNoInt)]<-paste0("adj",colnames(dFrameNoInt)[m])
+        }
 
         fullPV.df <- data.frame(fullPValues)
         fullPV.df <- t(fullPV.df)
@@ -199,7 +264,7 @@ for(t in taxaLevels )
         rownames(dFrameFull) <- names
         for (m in 2:dim(dFrameFull)[2]) {
             dFrameFull[,dim(dFrameFull)[2] + 1] <- p.adjust(dFrameFull[,m], method = "BH")
-            colnames(dFrameFull)[ncol(dFrameFull)]<-paste0("adj",colnames(dFrameFull)[m])
+           colnames(dFrameFull)[ncol(dFrameFull)]<-paste0("adj",colnames(dFrameFull)[m])
         }
 
 
@@ -211,7 +276,11 @@ for(t in taxaLevels )
 #	dFrame$adjPatient<-  p.adjust( dFrame$patientPValues, method = "BH" )
 
 
-	write.table(dFrame, file= paste( "pValuesLongitudinalSimpleModel_PatientA_", t, ".txt", sep=""), row.names=FALSE, sep="\t")
+	write.table(dFrame, file= paste( "pValuesLongitudinalSimpleModel_ALL_", t, ".txt", sep=""), row.names=FALSE, sep="\t")
+        write.table(dFrameBMI, file= paste( "pValuesLongBMIModel_ALL_", t, ".txt", sep=""), row.names=FALSE, sep="\t")
+        write.table(dFrameEnergy, file= paste( "pValuesLongEnergyModel_ALL_", t, ".txt", sep=""), row.names=FALSE, sep="\t")
+        write.table(dFrameNoInt, file = paste( "pValuesLongNoInt_ALL_",t, ".txt",sep=""), row.names=FALSE, sep="\t")
+        write.table(dFrameFull, file = paste("pValuesLongFullModel_ALL_", t, ".txt", sep=""), row.names=FALSE, sep="\t")
 
 #Temporarily just looking at one taxonomic level
 }
