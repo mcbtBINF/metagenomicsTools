76f282983451b01806a6fadec95cdbd33dae58e2
mcb1983 <mbrown67@uncc.edu>
2016-11-08 11:35:03 -0500

    First commit on playing with ggplot2

---
+++ src/scripts/mbrown67Viz/testggplot2.R
@@ -0,0 +1,6 @@
+## Purpose to explore ggplot2 in producing nice plots towards getting ready for better publication quality figures and better figures for my dissertation proposal and the ultimate dissertation
+
+rm(list=ls())
+
+library("ggplot2")
+## library("ggrepel")
